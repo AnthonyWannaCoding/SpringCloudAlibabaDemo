b3bc264de080bc834df3f09a9535b66afb338bc2
Anthony Zhang <anthonyzhang0721@gmail.com>
2023-09-09 13:45:39 +0800

    add sentinel server

--- borrowService/src/main/java/com/example/service/impl/BorrowServiceImpl.java
+++ borrowService/src/main/java/com/example/service/impl/BorrowServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.service.impl;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.example.entity.Book;
 import com.example.entity.Borrow;
 import com.example.entity.User;
@@ -11,6 +13,7 @@ import com.example.service.client.UserClient;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,6 +29,7 @@ public class BorrowServiceImpl implements BorrowService {
     BookClient bookClient;
 
     @Override
+    @SentinelResource(value = "details", blockHandler = "blocked")   //指定blockHandler，也就是被限流之后的替代解决方案，这样就不会使用默认的抛出异常的形式了
     public UserBorrowDetail getUserBorrowDetailByUid(int uid) {
         List<Borrow> borrow = mapper.getBorrowByUid(uid);
         User user = userClient.getUserById(uid);
@@ -34,5 +38,10 @@ public class BorrowServiceImpl implements BorrowService {
                 .map(b -> bookClient.getBookById(b.getBid()))
                 .collect(Collectors.toList());
         return new UserBorrowDetail(user, bookList);
+    }
+
+    //替代方案，注意参数和返回值需要保持一致，并且参数最后还需要额外添加一个BlockException
+    public UserBorrowDetail blocked(int uid, BlockException e) {
+        return new UserBorrowDetail(null, Collections.emptyList());
     }
 }
